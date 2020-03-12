e0f42c2afd527e4e86ed351ae1da22f6b45e2ef3
jedaport <davidpabon173@gmail.com>
2020-03-12 16:43:05 -0500

    Proyecto calculadora con spring boot

---
+++ src/test/java/kodiaj/calculatorwebspringboot/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package kodiaj.calculatorwebspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
